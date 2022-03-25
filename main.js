ace6fe9f96d2bbb4c910970ceb052e01c604bce5
Alexander Drapoy <dr4alex@gmail.com>
2022-03-25 21:01:57 +0200

    Початок

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeWithHover(element) {
+    let element = document.getElementById("box14") + "a";
+    document.getElementById.elemement.visibility = "visible";
+    
+}
+setTimeout(changeWithHover, 1000);
+
+
+
+
